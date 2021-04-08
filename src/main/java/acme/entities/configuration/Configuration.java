4176ec251f1108b8d8e4b779f8a5bd1aeeaff20f
ferangmor <ferangmor@alum.us.es>
2021-04-08 15:16:09 +0200

    Modelo Configuración spamwords

---
+++ src/main/java/acme/entities/configuration/Configuration.java
@@ -0,0 +1,27 @@
+package acme.entities.configuration;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Range;
+
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Configuration extends DomainEntity{
+	
+	private static final long	serialVersionUID	= 1L;
+	
+	
+	protected String			spamWords;
+	
+	@Range(min = 0, max = 1)
+	private double				spamThreshold;
+
+	@NotBlank
+	private String				language;
+}
